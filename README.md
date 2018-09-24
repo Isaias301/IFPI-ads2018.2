786a444f5d28e64c53b00ff4d925f3c6a3dd7e1e
Isaias Santana <isaiassantana301@gmail.com>
2018-09-24 20:26:56 -0300

    ADS 2018.2

---
+++ README.md
@@ -0,0 +1,1 @@
+# Curso superior em Analise e Desenvolvimento de Sistemas - IFPI - 2018.2
---
+++ atividade_@/ponto.py
@@ -0,0 +1,31 @@
+class Ponto():
+    """
+        Classe Ponto, usando como exemplo um plano cartesiano
+    """
+    # atributos
+    x = None
+    y = None
+
+    # comportamentos ou metódos
+    def mover(self, x, y):
+        self.x, self.y = x, y
+
+
+    def quadrante(self):
+        if self.x > 0 and self.y > 0:
+            return "Primeiro quadrante"
+        elif self.x < 0 and self.y > 0:
+            return "Segundo quadrante"
+        elif self.x < 0 and self.y < 0:
+            return "Terceiro quadrante"
+        elif self.x > 0 and self.y < 0:
+            return "Quarto quadrante"
+        elif self.x == 0 and self.y == 0:
+            return "Origem"
+
+ponto1 = Ponto()
+ponto1.x = 0
+ponto1.y = 0
+ponto1.mover(-0, -1)
+resultado = ponto1.quadrante()
+print(resultado)    
