740357abc837be7ca759e604204d41291088ad6d
Isaias Santana <isaiassantana301@gmail.com>
2018-10-30 20:20:10 -0300

    Classe view programada :-)

--- AtividadeI/model/view_model.py
+++ AtividadeI/model/view_model.py
@@ -1,2 +1,13 @@
 class View():
-    pass
+    largura = 0
+    altura = 0
+
+
+    def altura(self, altura):
+        if altura <= 100 or altura >= 0:
+            self.altura = altura
+    
+
+    def largura(self, largura):
+        if largura <= 100 or largura >= 0:
+            self.largura = largura
