1df79bb27a0b409e6819a5fc458bc409b2c650b1
Isaias Santana <isaiassantana301@gmail.com>
2018-10-30 20:25:26 -0300

    Novos comentarios e classe text_view programada

--- AtividadeI/model/text_view_model.py
+++ AtividadeI/model/text_view_model.py
@@ -1,2 +1,12 @@
-class TextView():
-    pass
+from view_model import View
+
+
+class TextView(View):
+    texto = None
+
+
+    def texto(self, texto):
+        """
+            Adiciona um texto na view
+        """
+        self.texto = texto
--- AtividadeI/model/view_model.py
+++ AtividadeI/model/view_model.py
@@ -4,10 +4,16 @@ class View():
 
 
     def altura(self, altura):
+        """
+            Adiciona uma altura e verifica se está dentro dos requisitos
+        """
         if altura <= 100 or altura >= 0:
             self.altura = altura
     
 
     def largura(self, largura):
+        """
+            Adiciona uma largura e verifica se está dentro dos requisitos
+        """
         if largura <= 100 or largura >= 0:
             self.largura = largura
