9801409013faf74f46f480bfe94e19e46f18f853
Isaias Santana <isaiassantana301@gmail.com>
2018-10-30 21:08:51 -0300

    Classe ViewGroup programada

--- AtividadeI/model/view_group_model.py
+++ AtividadeI/model/view_group_model.py
@@ -1,2 +1,13 @@
-class ViewGroup():
-    pass
+from view_model import View
+
+
+class ViewGroup(View):
+    layout = False
+    view = []
+
+
+    def view(self, view):
+        """
+            Adiciona views a Class ViewGroup. A mesma e um conjunto de view
+        """
+        self.view.append(view)
