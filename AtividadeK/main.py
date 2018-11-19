51a08fbd5956df5774cbb256a05bb71d3229bb26
Isaias Santana <isaiassantana301@gmail.com>
2018-11-19 12:11:04 -0300

    Criando classe cartao

---
+++ AtividadeK/main.py
@@ -0,0 +1,22 @@
+
+def main():
+    menu = """**********Teste**********
+    \n1 - Abrir conta
+    \nOpcao: 
+    """
+         
+    while True:
+        opcao = int(input(menu))
+
+        if opcao == 1:
+            titula = input("Nome: ")
+            cpf = input("CPF: ")
+            rg = input("RG: ")
+            conta = abrir_conta(titula, cpf, rg)
+            print(conta["mensagem"])
+        elif opcao == 0:
+            break
+
+
+if __name__ ==  "__main__":
+    main()
---
+++ AtividadeK/model/Cartao.py
@@ -0,0 +1,26 @@
+class Cartao:
+    __titulo = None
+
+
+    def __init__(self, titulo):
+        self.set_titulo(titulo)
+
+
+    def get_titulo(self):
+        return self.titulo
+
+
+    def set_titulo(self, titulo):
+        self.titulo = titulo
+
+
+    def arquivar():
+        pass
+
+
+    def alterar_data_entrega():
+        pass
+
+
+    def mover():
+        pass
